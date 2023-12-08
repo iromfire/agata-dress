368db56bb9b319946d1552f8a28cba68b8e18147
Артур Файзрахманов <a.faizrahmanov@solarlab.ru>
2023-12-08 18:21:53 +0300

    1) поправить файл инпут на телефоне 2) сжать сумки 3) добавить кнопки на главную 4)увеличить текст карточек

--- src/app/components/accessories/accessories.component.ts
+++ src/app/components/accessories/accessories.component.ts
@@ -2,35 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Handbag } from '../../domain/handbag';
 
 @Component({
-    selector: 'app-accessories',
-    templateUrl: './accessories.component.html',
-    styleUrls: ['./accessories.component.scss']
+  selector: 'app-accessories',
+  templateUrl: './accessories.component.html',
+  styleUrls: ['./accessories.component.scss'],
 })
 export class AccessoriesComponent implements OnInit {
-    handbags: Handbag[];
+  handbags: Handbag[];
 
-    ngOnInit() {
-        this.handbags = [
-            {
-                photo: 'BAG-1.png',
-                price: 8000
-            },
-            {
-                photo: 'BAG-2.png',
-                price: 8500
-            },
-            {
-                photo: 'BAG-3.png',
-                price: 7000
-            },
-            {
-                photo: 'BAG-4.png',
-                price: 9000
-            },
-            {
-                photo: 'BAG-5.png',
-                price: 6500
-            }
-        ];
-    }
+  ngOnInit() {
+    this.handbags = [
+      {
+        photo: 'BAG-1.jpg',
+        price: 8000,
+      },
+      {
+        photo: 'BAG-2.jpg',
+        price: 8500,
+      },
+      {
+        photo: 'BAG-3.jpg',
+        price: 7000,
+      },
+      {
+        photo: 'BAG-4.jpg',
+        price: 9000,
+      },
+      {
+        photo: 'BAG-5.jpg',
+        price: 6500,
+      },
+    ];
+  }
 }
--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,141 +1,164 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { noWhitespaceValidator } from '../../shared/validators/no-whitespace-validator';
-import { tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  pipe,
+  Subject,
+  take,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-    selector: 'app-order',
-    templateUrl: './order.component.html',
-    styleUrls: ['./order.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrderComponent implements OnInit {
-    private _botToken = '6931914478:AAEn0uhJpiGkM1FvNBtSROGpNu3A5jnfjQY';
-    private _chatId = '-1002086892379';
+export class OrderComponent implements OnInit, OnDestroy {
+  private _botToken = '6931914478:AAEn0uhJpiGkM1FvNBtSROGpNu3A5jnfjQY';
+  private _chatId = '-1002086892379';
 
-    form: FormGroup;
-    fieldMessages = {
-        name: 'имя',
-        phone: 'телефон',
-        description: 'описание заказа'
+  private _isButtonDisabled = new BehaviorSubject<boolean>(false);
+  private destroy$ = new Subject<void>();
+
+  form: FormGroup;
+  fieldMessages = {
+    name: 'имя',
+    phone: 'телефон',
+    description: 'описание заказа',
+  };
+  fromStylistCard: boolean;
+
+  isButtonDisabled = this._isButtonDisabled.asObservable();
+
+  get tooltipMessage(): string {
+    const invalidFields = this.getInvalidFields();
+    const formattedFields = invalidFields.map(
+      (field) => this.fieldMessages[field],
+    );
+    return `заполните: ${formattedFields.join(', ')}`;
+  }
+
+  constructor(
+    private _fb: FormBuilder,
+    private _http: HttpClient,
+    private _router: Router,
+    private _route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.form = this._fb.group({
+      name: [
+        null,
+        Validators.compose([Validators.required, noWhitespaceValidator()]),
+      ],
+      phone: [null, Validators.required],
+      description: [
+        null,
+        Validators.compose([Validators.required, noWhitespaceValidator()]),
+      ],
+      proportions: [],
+      photo: [],
+    });
+    this._route.data
+      .pipe(
+        tap((data) => {
+          this.fromStylistCard = data['fromStylistCard'];
+        }),
+      )
+      .subscribe();
+    this.isButtonDisabled.pipe(takeUntil(this.destroy$)).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  getInvalidFields(): string[] {
+    return Object.keys(this.form.controls).filter(
+      (field) => this.form.get(field).invalid,
+    );
+  }
+
+  submitOrder() {
+    this._isButtonDisabled.next(true);
+    const orderObservable = this.fromStylistCard
+      ? this.sendTelegramMessageWithPhoto()
+      : this.sendTelegramMessage();
+
+    orderObservable
+      .pipe(
+        tap(() => {
+          this._isButtonDisabled.next(false);
+          this._router.navigate(['order', 'success']);
+        }),
+        catchError((error) => {
+          this._isButtonDisabled.next(false);
+          return error;
+        }),
+      )
+      .subscribe();
+  }
+
+  sendTelegramMessage() {
+    const apiUrl = `https://api.telegram.org/bot${this._botToken}/sendMessage`;
+
+    const message = this.createTelegramMessage();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    const body = {
+      chat_id: this._chatId,
+      text: message,
     };
-    fromStylistCard: boolean;
 
-    get tooltipMessage(): string {
-        const invalidFields = this.getInvalidFields();
-        const formattedFields = invalidFields.map(
-            (field) => this.fieldMessages[field]
-        );
-        return `заполните: ${formattedFields.join(', ')}`;
+    return this._http.post(apiUrl, body, { headers });
+  }
+
+  sendTelegramMessageWithPhoto() {
+    const apiUrl = `https://api.telegram.org/bot${this._botToken}/sendPhoto`;
+
+    const message = this.createTelegramMessage();
+
+    const formData = new FormData();
+    formData.append('chat_id', this._chatId);
+    formData.append('caption', message);
+    formData.append('photo', this.form.get('photo').value);
+
+    return this._http.post(apiUrl, formData);
+  }
+
+  createTelegramMessage(): string {
+    const name = this.form.get('name').value;
+    const phone = `+7${this.form.get('phone').value}`;
+    const description = this.form.get('description').value;
+    const proportions = this.form.get('proportions').value;
+
+    if (this.fromStylistCard) {
+      return `ЗАЯВКА ОНЛАЙН СТИЛИСТ! \nИмя: ${name}\nНомер телефона: ${phone}\nОписание заказа: ${description} \nПараметры: ${proportions}`;
+    } else {
+      return `НОВЫЙ ЗАКАЗ! \nИмя: ${name}\nНомер телефона: ${phone}\nОписание заказа: ${description}`;
     }
+  }
 
-    constructor(
-        private _fb: FormBuilder,
-        private _http: HttpClient,
-        private _router: Router,
-        private _route: ActivatedRoute
-    ) {}
-
-    ngOnInit() {
-        this.form = this._fb.group({
-            name: [
-                null,
-                Validators.compose([
-                    Validators.required,
-                    noWhitespaceValidator()
-                ])
-            ],
-            phone: [null, Validators.required],
-            description: [
-                null,
-                Validators.compose([
-                    Validators.required,
-                    noWhitespaceValidator()
-                ])
-            ],
-            proportions: [],
-            photo: []
-        });
-        this._route.data
-            .pipe(
-                tap((data) => {
-                    this.fromStylistCard = data['fromStylistCard'];
-                })
-            )
-            .subscribe();
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.form.patchValue({
+        photo: file,
+      });
     }
-
-    getInvalidFields(): string[] {
-        return Object.keys(this.form.controls).filter(
-            (field) => this.form.get(field).invalid
-        );
-    }
-
-    submitOrder() {
-        const orderObservable = this.fromStylistCard
-            ? this.sendTelegramMessageWithPhoto()
-            : this.sendTelegramMessage();
-
-        orderObservable
-            .pipe(
-                tap(() => {
-                    this._router.navigate(['order', 'success']);
-                })
-            )
-            .subscribe();
-    }
-
-    sendTelegramMessage() {
-        const apiUrl = `https://api.telegram.org/bot${this._botToken}/sendMessage`;
-
-        const message = this.createTelegramMessage();
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json'
-        });
-
-        const body = {
-            chat_id: this._chatId,
-            text: message
-        };
-
-        return this._http.post(apiUrl, body, { headers });
-    }
-
-    sendTelegramMessageWithPhoto() {
-        const apiUrl = `https://api.telegram.org/bot${this._botToken}/sendPhoto`;
-
-        const message = this.createTelegramMessage();
-
-        const formData = new FormData();
-        formData.append('chat_id', this._chatId);
-        formData.append('caption', message);
-        formData.append('photo', this.form.get('photo').value);
-
-        return this._http.post(apiUrl, formData);
-    }
-
-    createTelegramMessage(): string {
-        const name = this.form.get('name').value;
-        const phone = `+7${this.form.get('phone').value}`;
-        const description = this.form.get('description').value;
-        const proportions = this.form.get('proportions').value;
-
-        if (this.fromStylistCard) {
-            return `ЗАЯВКА ОНЛАЙН СТИЛИСТ! \nИмя: ${name}\nНомер телефона: ${phone}\nОписание заказа: ${description} \nПараметры: ${proportions}`;
-        } else {
-            return `НОВЫЙ ЗАКАЗ! \nИмя: ${name}\nНомер телефона: ${phone}\nОписание заказа: ${description}`;
-        }
-    }
-
-    onFileSelected(event: any) {
-        const file = event.target.files[0];
-
-        if (file) {
-            this.form.patchValue({
-                photo: file
-            });
-        }
-    }
+  }
 }
